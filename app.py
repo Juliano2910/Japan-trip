04df85a62157be1d97eadef6ca636769b912286d
Juliano Voland <julianovoland@MacBook-Air-de-Juliano.local>
2025-04-28 15:05:20 -0400

    Ajout du vrai contenu dans app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Voyage au Japon 🎌", page_icon="🎎", layout="wide")
+
+st.title("🎂 Joyeux anniversaire !")
+st.write("Bienvenue sur ton site spécial pour ton voyage au Japon 🇯🇵✈️ !")
+
